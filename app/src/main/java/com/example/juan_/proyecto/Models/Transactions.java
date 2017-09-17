89ec88698b24bc89356bc68fbbb001bae2637e98
juanlara666 <talitakumi666@gmail.com>
2017-09-17 15:49:48 -0500

    Initial commit

---
+++ app/src/main/java/com/example/juan_/proyecto/Models/Transactions.java
@@ -0,0 +1,64 @@
+package com.example.juan_.proyecto.Models;
+
+import java.util.Date;
+
+/**
+ * Created by juan_ on 11/09/2017.
+ */
+
+public class Transactions {
+    protected int id;
+    protected double amount;
+    protected Date date;
+    protected int user_id;
+    protected String tittle;
+    protected String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ app/src/main/java/com/example/juan_/proyecto/Models/User.java
@@ -0,0 +1,44 @@
+package com.example.juan_.proyecto.Models;
+
+/**
+ * Created by juan_ on 11/09/2017.
+ */
+
+public class User {
+    protected int id;
+    protected String username;
+    protected String password;
+    protected String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
